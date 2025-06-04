355926ae3a4705ed78ad384d354149cc129f42e6
br1an17 <brianzairpaez@gmail.com>
2025-06-04 19:42:48 -0300

    subimos el ptoyecto de la preentrega

---
+++ index.js
@@ -0,0 +1,52 @@
+
+const [, , method, resource, ...params] = process.argv;
+const URL = 'https://fakestoreapi.com';
+
+//Consultar todos los productos
+async function getAllProducts() {
+  const response = await fetch(`${URL}/products`);
+  const data = await response.json();
+  console.log(data);
+}
+
+// Consultar un producto
+async function getProductById(id) {
+  const response = await fetch(`${URL}/products/${id}`);
+  const data = await response.json();
+  console.log(data);
+}
+
+// Crear un producto
+async function createProduct(title, price, category) {
+  const response = await fetch(`${URL}/products`, {
+    method: 'POST',
+    body: JSON.stringify({ title, price: parseFloat(price), category }),
+    headers: { 'Content-Type': 'application/json' }
+  });
+  const data = await response.json();
+  console.log(data);
+}
+
+// eliminar un producto
+async function deleteProduct(id) {
+  const response = await fetch(`${URL}/products/${id}`, {
+    method: 'DELETE'
+  });
+  const data = await response.json();
+  console.log(data);
+}
+
+if (method === 'GET' && resource === 'products' && params.length === 0) {
+  await getAllProducts();
+} else if (method === 'GET' && resource.startsWith('products/')) {
+  const id = resource.split('/')[1];
+  await getProductById(id);
+} else if (method === 'POST' && resource === 'products') {
+  const [title, price, category] = params;
+  await createProduct(title, price, category);
+} else if (method === 'DELETE' && resource.startsWith('products/')) {
+  const id = resource.split('/')[1];
+  await deleteProduct(id);
+} else {
+  console.log('Comando no reconocido.');
+}
