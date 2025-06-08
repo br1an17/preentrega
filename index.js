8466257dba3959ca6b75e2aac21d3e38758a452f
br1an17 <brianzairpaez@gmail.com>
2025-06-08 15:25:54 -0300

    cambiamos a funciones flecha

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
-
 const [, , method, resource, ...params] = process.argv;
-const URL = 'https://fakestoreapi.com';
+const URL = "https://fakestoreapi.com";
 
 //Consultar todos los productos
-async function getAllProducts() {
+const getAllProducts = async () => {
   const res = await fetch(`${URL}/products`);
   const data = await res.json();
   console.log(data);
-}
+};
 
 // Consultar un producto
-async function getProductById(id) {
+const getProductById = async (id) => {
   const res = await fetch(`${URL}/products/${id}`);
   const data = await res.json();
   console.log(data);
-}
+};
 
 // Crear un producto
-async function createProduct(title, price, category) {
+const createProduct = async (title, price, category) => {
   const res = await fetch(`${URL}/products`, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify({ title, price: parseFloat(price), category }),
-    headers: { 'Content-Type': 'application/json' }
+    headers: { "Content-Type": "application/json" },
   });
   const data = await res.json();
   console.log(data);
-}
+};
 
 // eliminar un producto
-async function deleteProduct(id) {
+const deleteProduct = async (id) => {
   const res = await fetch(`${URL}/products/${id}`, {
-    method: 'DELETE'
+    method: "DELETE",
   });
   const data = await res.json();
   console.log(data);
-}
+};
 
-if (method === 'GET' && resource === 'products' && params.length === 0) {
+if (method === "GET" && resource === "products" && params.length === 0) {
   await getAllProducts();
-} else if (method === 'GET' && resource.startsWith('products/')) {
-  const id = resource.split('/')[1];
+} else if (method === "GET" && resource.startsWith("products/")) {
+  const id = resource.split("/")[1];
   await getProductById(id);
-} else if (method === 'POST' && resource === 'products') {
+} else if (method === "POST" && resource === "products") {
   const [title, price, category] = params;
   await createProduct(title, price, category);
-} else if (method === 'DELETE' && resource.startsWith('products/')) {
-  const id = resource.split('/')[1];
+} else if (method === "DELETE" && resource.startsWith("products/")) {
+  const id = resource.split("/")[1];
   await deleteProduct(id);
 } else {
-  console.log('Comando no reconocido.');
+  console.log("Comando no reconocido.");
 }
